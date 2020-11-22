1ab60de13b6386fd3d97d901d566c9bf4da78a3f
Rodolfo Gomes <rodolfogomes.dev@gmail.com>
2020-11-22 19:43:16 -0300

    Primeiro comit

---
+++ README.txt
@@ -0,0 +1,11 @@
+passo 1: 
+
+Abrir o diretório:
+
+exemplo: cd C:\Users\user\my-angular-test
+
+passo 2: executar comando ng serve --port 333 -o
+
+passo 3: tela principal se encontra em:
+
+C:\Users\user\my-angular-test\src\app\views\home\home.component.html
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
